834e2afc502f37bf9948581fdb973a0a8f994bcc
sanjay <sanjayachari@argonquber.com>
2023-12-02 23:14:26 +0530

    free project

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -10,8 +10,8 @@ const Cv = () => {
 
     <div className=''>
 
-    <button className='hover:bg-[white] hover:text-black border border-white duration-700 h-[30px] md:h-[40px] w-[110px] md:w-[140px] m-1  bg-[black] text-white rounded shadow-lg'><a href={mernStackRes} download alt='resume'  className=' '>Download CV</a></button>
     <button className='hover:bg-[white] hover:text-black border border-white duration-700 h-[30px] md:h-[40px] w-[110px] md:w-[140px] m-1  bg-[black] text-white rounded shadow-lg'><a href='#contact' className=' '> Contact Me! </a></button>
+    <button className='hover:bg-[white] hover:text-black border border-white duration-700 h-[30px] md:h-[40px] w-[110px] md:w-[140px] m-1  bg-[black] text-white rounded shadow-lg'><a href={mernStackRes} download alt='resume'  className=' '>6371290586</a></button>
 
     </div>
 
